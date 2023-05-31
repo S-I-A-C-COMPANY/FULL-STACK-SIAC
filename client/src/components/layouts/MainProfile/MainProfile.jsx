c0b05b826567207feb98b5ee13b1b7539071edd0
Jackson Londoño <alexisjacksonlon213@gmail.com>
2023-05-31 15:57:47 -0500

    Se arreglo error en MainProfile

--- client/src/components/layouts/MainProfile/MainProfile.jsx
+++ client/src/components/layouts/MainProfile/MainProfile.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { ContentProfile } from '../ContentProfile/ContentProfile'
 import { SideBarProfile } from '../SideBarProfile/SideBarProfile'
 
-import React from 'react';
 
 export const MainProfile = () => {
   return (
